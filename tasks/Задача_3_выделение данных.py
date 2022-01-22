a5af5f2c280f089adfe1e7b733c6b9c2440d951c
T-Centr <T-Centr@yandex.ru>
2022-01-22 15:07:39 +0400

    добавлено описание решения задачи_3 в файле readme

--- tasks/Задача_3_выделение данных.py
+++ tasks/Задача_3_выделение данных.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 
-data = pd.read_csv("https://video.ittensive.com/python-advanced/"
-                   "data-9753-2019-07-25.utf.csv", delimiter=";")
-data["Sum"] = data.apply(lambda x: 100*x[6]/x[7], axis=1)
-data = data[data["Sum"] < 2]
+data = pd.read_csv(
+    "https://video.ittensive.com/python-advanced/data-9753-2019-07-25.utf.csv",
+    delimiter=";")
+data["Percent"] = data.apply(
+    lambda x: 100*x['UnemployedDisabled'] / x['UnemployedTotal'], axis=1)
+data = data[data["Percent"] < 2]
 data = data.set_index("Year")
 data = data.sort_index()
 print(data.index[0:1])

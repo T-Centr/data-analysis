33f0e5dbdcaa538dfbb9ec97a8bc5ec2f570ec30
T-Centr <T-Centr@yandex.ru>
2022-01-26 13:08:23 +0400

    добавлено описание решения задачи_4 в файле readme

--- tasks/Задача_2_Объединение данных и сортировка по индексам.py
+++ tasks/Задача_2_Объединение данных и сортировка по индексам.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 
-data1 = pd.read_csv("https://video.ittensive.com/"
-                    "python-advanced/data-9753-2019-07-25.utf.csv",
-                    delimiter=";")
+data1 = pd.read_csv(
+    "https://video.ittensive.com/python-advanced/data-9753-2019-07-25.utf.csv",
+    delimiter=";"
+)
 data1 = data1.set_index(["Year", "Period"])
 
-data2 = pd.read_csv("https://video.ittensive.com/python-advanced/"
-                    "data-5283-2019-10-04.utf.csv", delimiter=";")
+data2 = pd.read_csv(
+    "https://video.ittensive.com/python-advanced/data-5283-2019-10-04.utf.csv",
+    delimiter=";"
+)
 data2 = data2.set_index(["AdmArea", "Year", "Month"])
 data2 = data2.loc["Центральный административный округ"]
 data2.index.names = ["Year", "Period"]
--- tasks/Задача_3_выделение данных.py
+++ tasks/Задача_3_выделение данных.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 data = pd.read_csv(
     "https://video.ittensive.com/python-advanced/data-9753-2019-07-25.utf.csv",
-    delimiter=";")
+    delimiter=";"
+)
 data["Percent"] = data.apply(
     lambda x: 100*x['UnemployedDisabled'] / x['UnemployedTotal'], axis=1)
 data = data[data["Percent"] < 2]
